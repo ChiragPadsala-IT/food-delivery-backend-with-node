84b16573caf0a407884b48ec02330b15f89c2531
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2026-05-11 10:48:42 -0400

    fix middleware if empty json body comming

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,13 +2,16 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
   try {
-    // Get Token
+    if (!req.body) req.body = {};
 
+    // Get Token
     const token = req.headers["authorization"].split(" ")[1];
 
     const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
     const payload = jwt.verify(token, JWT_SECRET_KEY);
+
+    console.log(`${payload.id}`);
 
     req.body.user_id = payload.id;
 
