67b4d0abac705a4d33863d71aa7299562c97fdba
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2026-05-16 02:32:20 -0400

    docker implemented

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+// don env configuration
+const path = require("path");
+const dotenv = require("dotenv");
+const envPath = path.join(__dirname, ".env");
+dotenv.config({ path: envPath });
+
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const colors = require("colors");
-const dotenv = require("dotenv");
-const path = require("path");
 const connectDB = require("./src/config/db.js");
 
 const authRoute = require("./src/routes/auth.route.js");
 const userRoute = require("./src/routes/user.route.js");
 const restaurantRoute = require("./src/routes/restaurant.route.js");
-
-// don env configuration
-const envPath = path.join(__dirname, ".env");
-dotenv.config({ path: envPath });
 
 // DB connection
 connectDB();
@@ -35,6 +35,6 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/restaurant", restaurantRoute);
 
 // app listen
-app.listen(PORT, () => {
+app.listen(3000, () => {
   console.log(`Service running on ${PORT}`.bgGreen);
 });
